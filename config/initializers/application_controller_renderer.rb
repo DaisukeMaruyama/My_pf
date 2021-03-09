26cd0c59dbced90f2bed632cd78844de8f288ada
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-09 05:56:15 +0000

    [Update]refileシークレットキー記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c878ecbfcd5edf1a535432ab2aae184cfa09ed8cf61884e62428cd5555c073d5cde764ef4acd1d5f464fcb687c9eb5d69aecd57bdef675c094fe46415b06eeda'
