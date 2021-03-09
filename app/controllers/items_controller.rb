12d4d09f62f0e923e916c70aadaca07e513424f8
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-09 05:59:11 +0000

    [Update]製品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
   
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
     @genres = Genre.all
   end
 
   def show
-    
+    @item = Itme.find(params[:id])
   end
   
 end
