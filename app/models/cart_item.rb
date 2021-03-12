596599c8f8b58252fb442728f39b838b88266f51
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-12 00:45:59 +0000

    [Update]紐付け・カート機能実装コード

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,11 @@
 class CartItem < ApplicationRecord
+  
+  belongs_to :item
+  belongs_to :user
+  
+ 
+  def subtotal_price
+    (amount * item.price)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :cart_items, dependent: :destroy
+  
+     # カートアイテム金額合計
+  def cart_item_sum
+    total = 0
+    cart_items.each do |cart_item|
+      total += cart_item.subtotal_price
+    end
+    total
+  end
+
+  # カートアイテム個数合計
+  def cart_total_count
+    amount = 0
+    cart_items.each do |cart_item|
+      am += cart_item.amount
+    end
+    amount
+  end
+  
+         
 end
