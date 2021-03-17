26ceae22502dc6381f988be0c4bc2a44ff300d4d
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-17 12:51:30 +0000

    [Update]注文詳細

---
+++ db/migrate/20210317040503_change_column_to_order_details.rb
@@ -0,0 +1,4 @@
+class ChangeColumnToOrderDetails < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_110320) do
+ActiveRecord::Schema.define(version: 2021_03_17_040503) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "user_id"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2021_03_14_110320) do
     t.text "introduction"
     t.decimal "price", precision: 8, scale: 2
     t.boolean "is_active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.decimal "price"
+    t.integer "amount"
+    t.integer "making_status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
