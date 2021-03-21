f42f1899e57b4e5a6eb0663ebc24d2a1e04e7ef6
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-21 09:14:06 +0000

    [Update]パスワード変更機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -7,6 +7,7 @@ class Public::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      sign_in(@user, :bypass => true)#パスワードを変えたときにログインキープ
       flash[:notice] = "You have updated your information"
       redirect_to user_path
     else
@@ -24,10 +25,14 @@ class Public::UsersController < ApplicationController
   def withdraw
   end
   
+  def newpassword
+    @user = User.find(params[:id])
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :address, :city, :country, :postal_code)
+    params.require(:user).permit(:first_name, :last_name, :email, :address, :city, :country, :postal_code, :password, :password_confirmation)
   end
   
 end
