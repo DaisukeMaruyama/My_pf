1eb8d7dc816e761cb05a7ad85c184159fa65e9fa
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-25 12:41:56 +0000

    [Update]ヘッダーへジャンル検索ワード配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :get_genre_all
 
   protected
+  
+  def get_genre_all
+    @genres = Genre.all
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :postal_code, :phone_number])
