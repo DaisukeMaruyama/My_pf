fc1f11211d30e28d7124a27dc8fec543a4b87b7e
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-26 06:23:50 +0000

    [Update]コントローラーアクション記述

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,14 +5,39 @@ class Admin::UsersController < ApplicationController
   end
   
   def update
-  
+    @user = User.find(params[:id])
+    if @user.update(admin_user_params)
+      flash[:notice] = "会員情報の編集を保存しました。"
+      redirect_to admin_users_path(user)
+    else
+      render :edit
+    end  
   end
   
   def edit
-    
+    @user = User.find(params[:id])
   end
   
   def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+  
+  def admin_user_params
+    params.require(:user).permit(
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :city,
+      :country,
+      :postal_code,
+      :address,
+      :email,
+      :is_deleted,
+      :password
+      )
   end
   
 end
