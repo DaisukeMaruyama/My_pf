bd78da42643643a49566a5932bb385e4bc685234
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-03-30 01:58:38 +0000

    [Update]subjctのenum追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ApplicationRecord
   validates :email, presence: true, length: {maximum:255},format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :message, presence: true
+  validates :subject, presence: true
+  
+  enum subject: {"---"=>0, "Delivery"=> 1, "Order Issues"=> 2 , "Product & Stock"=> 3 , "Others"=> 4 }
   
 end
