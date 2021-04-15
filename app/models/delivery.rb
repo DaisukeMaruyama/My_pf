db5839252bbc5d5d3dea315f138999fa97fade70
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-04-15 08:40:02 +0000

    [Update]バリデーション修正

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,21 +1,21 @@
 class Delivery < ApplicationRecord
-  
-  validates :postal_code, presence: true
-  validates :address, length: {minimum: 2, maximum: 100}, presence: true
-  validates :city, presence: true
-  validates :country, presence: true
+
+  validates :delivery_postal_code, presence: true
+  validates :delivery_address, length: {minimum: 2, maximum: 100}, presence: true
+  validates :delivery_city, presence: true
+  validates :delivery_country, presence: true
 	validates :last_name, length: {minimum: 1}, presence: true
 	validates :first_name, length: {minimum: 1}, presence: true
-  
+
   belongs_to :user
-  
+
   def full_shipping_address
     self.first_name + "　" + self.last_name + "　" + self.delivery_address + "　" +  self.delivery_city + "　" + self.delivery_postal_code + "　" + self.country_name
   end
-  
+
   def country_name
     c = ISO3166::Country[self.delivery_country]
     return c.translations[I18n.locale.to_s] || c.name
   end
-  
+
 end
