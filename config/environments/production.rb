7a4e98223f009143a87e00f8abe3842eea277352
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-04-21 04:04:22 +0000

    デプロイエラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.public_file_server.enabled = true 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
@@ -24,8 +24,8 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
+  config.assets.css_compressor = :sass # sass-rails gemを使用している場合コメントアウトを外す
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
