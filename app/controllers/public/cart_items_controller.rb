0626615e4f6ab8ec77f6ed96ec56390ace282bd9
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-05-03 13:51:16 +0000

    [Update]カートアイテムに追加した物がすでにカートにある場合別製品として扱わないように修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,8 +9,17 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_user.cart_items.new(cart_item_params)
-    @cart_item.save!
-    redirect_to cart_items_path
+    if @cart_item = current_user.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      @cart_item.save
+      flash[:notice] = "You have added the item to your shopping cart"
+      redirect_to cart_items_path
+    else 
+      @cart_item = current_user.cart_items.new(cart_item_params)
+      @cart_item.save!
+      flash[:notice] = "You have added the item to your shopping cart"
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
